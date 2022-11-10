95cc4bc6cd4641c6c5c077ed2d9e57a0183d7136
Samuele Catalano <csamuele73@mail.com>
2022-11-10 18:36:01 +0100

    init commit

---
+++ src/main/java/com/controllerprotection/controllerprotection/ControllerprotectionApplication.java
@@ -0,0 +1,13 @@
+package com.controllerprotection.controllerprotection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerprotectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerprotectionApplication.class, args);
+	}
+
+}
